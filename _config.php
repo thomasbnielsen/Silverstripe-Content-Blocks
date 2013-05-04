5963437222cbda866f0c462f35cfbeec7cefd65c
Thomas B. Nielsen <mail@nobrainer.dk>
2013-05-04 14:42:09 +0200

    Remove

--- _config.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Extend all pages with the section module
-Object::add_extension('Page', 'SectionModule');
-// Add custom styles to the CMS / Admin area
-LeftAndMain::require_css('sectionmodule/css/SectionModule.css');
-?>

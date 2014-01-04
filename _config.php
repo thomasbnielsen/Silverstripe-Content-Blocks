794c6f4b127166aa90938ade2eb9eddb0d0baa37
Thomas B. Nielsen <mail@nobrainer.dk>
2014-01-04 14:50:09 +0100

    Use YML for configuration and extension

--- _config.php
+++ _config.php
@@ -1,6 +1,2 @@
 <?php
 define('SECTION_MODULE_DIR',basename(dirname(__FILE__)));
-// Extend all pages with the section module
-Object::add_extension('Page', 'SectionModule');
-// Add custom styles to the CMS / Admin area
-LeftAndMain::require_css('sectionmodule/css/SectionModule.css');
